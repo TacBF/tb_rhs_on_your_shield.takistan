56ddcd7cfcc863f6f1ca6d75b500c8c2b4345625
Ballista Milsim <ballista.milsim@gmail.com>
2017-04-17 16:42:57 +0200

    tb_rhs_on_your_shield.takistan mission added

---
+++ briefing_blue.hpp
@@ -0,0 +1,21 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+<titleFont>Gerneral Reminders:</titleFont><br />
+<*> <green>Squad leaders</green> - take the SL kit and deploy <RP> Rally Points<br />
+<*> Help out your team by deploying <FO> Forward Outposts<br />
+<br />
+
+<titleFont>Deployment Options:</titleFont><br />
+<boxes> Forward Base<br />
+<FO> Forward Outposts<br />
+<RP> Team Rally Points<br />
+<br />
+
+<titleFont>Strategies:</titleFont><br />
+<*> Take and hold the high ground!<br />
+<*> All the OPs are mutually supporting and make good positions to cover the others.<br />
+<*> Defend in depth!<br />
+<*> Helicopter support, fancy optics and heavy weapons are out of the picture due to COIN posturing and/or budget cuts.<br />
+<*> It takes 1 minute to get to Khushab on the north road and 3 minutes on the south road.<br />
+<*> Whatever you do, get organised FAST, get things done FAST and STICK IT TO THE ENEMY!<br />
+<br />
+
---
+++ briefing_red.hpp
@@ -0,0 +1,17 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+<titleFont>Gerneral Reminders:</titleFont><br />
+<*> <green>Squad leaders</green> - take the SL kit and deploy <RP> Rally Points<br />
+<*> Help out your team by deploying <FO> Forward Outposts<br />
+<br />
+
+<titleFont>Deployment Options:</titleFont><br />
+<FO> Forward Outposts<br />
+<br />
+
+<titleFont>Strategies:</titleFont><br />
+<*> Take and hold the high ground!<br />
+<*> All the OPs are mutually supporting and make good positions to cover the others. Watch out for crossfire!<br />
+<*> The Americans have two roads to get to the AO and only have ground transport.<br />
+<*> Attack in depth!<br />
+<*> Whatever you do, get organised FAST, get things done FAST and STICK IT TO THE ENEMY!<br />
+<br />
